06ebaaa905bf10850e7072c91a654c80994f0348
ShoxruxCode <zokirjonovshoxrux99@gmail.com>
2024-03-06 20:41:37 +0500

    Teach Comparison

--- Program.cs
+++ Program.cs
@@ -8,9 +8,12 @@ string ageAsString = Console.ReadLine();
 Console.WriteLine("Converting...");
 int age = Convert.ToInt32(ageAsString);
 Console.WriteLine($"Successfully converted! {age}");
-int KeschasAge = 2;
-int ageDifference = age - KeschasAge;
-Console.WriteLine($"Men KESCHAdan {ageDifference} yosh kattaman");
-Console.WriteLine($"KESCHA mendan {ageDifference} yosh kichik");
-Console.WriteLine($"Men va KESCHAni oramizdagi yosh tafovut {ageDifference} ga teng");
-// Uyga vazifa bu Kescha va userning yoshlari orasidagi tafovutni hisoblash
+int keschasAge = 2;
+int ageDifference = age - keschasAge;
+Console.WriteLine($"The difference between your and Kescha's age is {ageDifference}");
+Console.WriteLine($"You are older than KESCHA: {age > keschasAge}");
+Console.WriteLine($"You are younger than KESCHA: {age < keschasAge}");
+Console.WriteLine($"You are older than or equal to KESCHA: {age >= keschasAge}");
+Console.WriteLine($"You are younger than or equal to KESCHA: {age <= keschasAge}");
+Console.WriteLine($"You are not equal to KESCHA: {age != keschasAge}");
+Console.WriteLine($"You are equal to KESCHA: {age == keschasAge}");
