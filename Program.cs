9569178194277b9c0a885447dd62343468e6f576
ShoxruxCode <zokirjonovshoxrux99@gmail.com>
2024-04-26 17:57:21 +0500

    INFRA : Upload Project

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
 using KESCHA.Classes;
 
-Animal kescha = new Animal(name: "Kescha", age: 3);
+namespace KESCHA
+{
+    class Program
+    {
+        static void Main()
+        {
+            Animal kescha = new Animal(name: "Kescha", age: 3);
 
-Console.Write("Enter your name: ");
-string userName = Console.ReadLine();
-kescha.Greet(userName);
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            kescha.Greet(userName);
 
-Console.Write("Enter your age: ");
-int userAge = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your age: ");
+            int userAge = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Converting...");
-Console.WriteLine($"Successfully converted! {userAge}");
+            Console.WriteLine("Converting...");
+            Console.WriteLine($"Successfully converted! {userAge}");
 
-kescha.CalculateAgeDifferenceWithNoReturns(userAge);
-kescha.Print();
-kescha.CompareAges(userAge);
-kescha.TellAboutFriends(userName, userAge);
+            kescha.CalculateAgeDifferenceWithNoReturns(userAge);
+            kescha.Print();
+            kescha.CompareAges(userAge);
+            kescha.TellAboutFriends(userName, userAge);
+        }
+    }
+}
