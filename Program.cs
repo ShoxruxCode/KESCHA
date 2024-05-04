f65c04f76a3495362f60e23d5b60f30406234749
ShoxruxCode <zokirjonovshoxrux99@gmail.com>
2024-05-04 22:52:22 +0500

    INFRA : OOP bo’limi uchun 1-amaliy vazifa

--- Program.cs
+++ Program.cs
@@ -1,62 +1,24 @@
-﻿using System;
-// Basics.Takrorlash bo’limi uchun 1-amaliy vazifa
-// 1-topshiriq
-// 1 dan 1000 gacha bo’lgan toq sonlarni yig’indisini ekranga chiqaradigan dastur
-// tuzing. While loop dan foydalaning!
-int number = 1;
-int sum = 0;
-while(number <= 1000)
+﻿using KESCHA.Classes;
+namespace KESCHA
 {
-    if(number % 2 != 0)
+    class Program
     {
-        sum = sum + number;
-    }
-    number++;
-}
-Console.WriteLine(sum);
+        static void Main()
+        {   
+            Book book= new Book(name: "Kiob nomi");
 
-// 2-topshiriq
-// Array ichidagi eng katta va eng kichik qiymatlarni ko’paytmasini ekranga
-// chiqaradigan dastur tuzing. For loop dan foydalaning!
-Console.Write("Array uzunligini kiriting: ");
-int length = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Array qiymatlarini kiriting (butun sonda): ");
-int[] array = new int[length];
-for(int i = 0; i < length; i++)
-{
-    Console.Write($"array[{i}]=");
-    array[i] = Convert.ToInt32(Console.ReadLine());
-}
-int maximumValueOfArray = array[0];
-int minimumValueOfArray = array[0];
-for(int i = 0; i < array.Length; i++)
-{
-    if(array[i] > maximumValueOfArray)
-    {
-        maximumValueOfArray = array[i];
+            Console.Write("Kitob nomini kiriting: ");
+            string name = Console.ReadLine();
+            book.NameBook = name;
+
+            Console.Write("Kitob muallifini kiriting: ");
+            string muallif = Console.ReadLine();
+
+            Console.Write("Kitob narxini kiriting: ");
+            int narxi = Convert.ToInt32(Console.ReadLine());
+
+            book.KitobHaqida(muallif, narxi);
+            book.KitobMuallifiHaqida(muallif);
+        }
     }
-    if(array[i] < minimumValueOfArray)
-    {
-        minimumValueOfArray = array[i];
-    }
-}
-Console.WriteLine($"Arrayning eng katta va eng kichik qiymatlarining ko'paytmasi: ");
-Console.WriteLine($"{maximumValueOfArray} * {minimumValueOfArray} = {maximumValueOfArray * minimumValueOfArray}");
-// 3-topshiriq
-// Consolga int tipidagi butun son kiritilgan => int x
-// x! (x factorial) ni hisoblaydigan dastur tuzing.
-Console.Write("Factorial qiymatini bilmoqchi bo'lgan soningizni kiriting (musbat va butun qiymat kiriting): ");
-int x = Convert.ToInt32(Console.ReadLine());
-if(x < 0)
-{
-    Console.WriteLine("Siz manfiy qiymat kiritdingiz!");
-}
-else
-{
-    long factorial = 1;
-    for(int i = 1; i <=x; i++)
-    {
-        factorial *= i;
-    }
-    Console.WriteLine($"{x}! = {factorial}");
 }
